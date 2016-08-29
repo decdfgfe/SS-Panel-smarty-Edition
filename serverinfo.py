8b6b7b73e9584d7632f7c71e67766857cadce136
kot4ri <kot4ri@gmail.com>
2016-08-29 16:34:30 +0800

    Update serverinfo.py
    修复部分vps上节点监控易崩溃的问题，xiaoxin提供

--- serverinfo.py
+++ serverinfo.py
@@ -1,6 +1,6 @@
 #coding=utf8
 
-posturl = 'http://your.domain.com/serverinfo.php'
+posturl = 'http://your.domain.name/serverinfo.php'
 server_id = 1
 
 import sys
@@ -12,7 +12,7 @@ import psutil
 import urllib
 import urllib2
 
-def postdata(load, cpu, u, d):
+def postdata(load, cpu, u, d, n):
     para_data = {}
     para_data['id']=server_id
     para_data['method']='update'
@@ -20,12 +20,30 @@ def postdata(load, cpu, u, d):
     para_data['cpu']=cpu
     para_data['upload']=u
     para_data['download']=d
+    para_data['connections']=n
+    print '发送的数据:'+str(para_data)
     para_data=urllib.urlencode(para_data)
-    f = urllib2.urlopen(posturl, para_data)
-    content=f.read()
-    return content
-
-def bytes2human(n):    
+    
+    f = urllib2.Request(posturl, data=para_data)
+    f.add_header('User-Agent', 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:26.0) Gecko/20100101 Firefox/26.0')
+    try:
+        f = urllib2.urlopen(f,timeout=3)
+        f.read(1)
+    except urllib2.URLError, e: 
+        if hasattr(e,"reason"):
+            print "Failed to reach the server"
+            print "The reason:",e.reason
+        elif hasattr(e,"code"):
+            print "The server couldn't fulfill the request"
+            print "Error code:",e.code
+            print "Return content:",e.read()
+    else:
+        pass
+    del f
+    
+    return ""
+    
+def bytes2human(n):
         """   
         >>> bytes2human(10000)   
         '9.8 K'   
@@ -42,18 +60,48 @@ def bytes2human(n):
                         return '%.2f %s' % (value, s)    
         return '%.2f B' % (n)
         
+
 def load_stat():
     loadavg = {}
-    f = open("/proc/loadavg")
-    con = f.read().split()
-    f.close()
-    loadavg['lavg_1']=con[0]
-    loadavg['lavg_5']=con[1]
-    loadavg['lavg_15']=con[2]
-    loadavg['nr']=con[3]
-    loadavg['last_pid']=con[4]
+    if os.path.exists("/proc/loadavg")  == True:
+        f = open("/proc/loadavg")
+        con = f.read().split()
+        f.close()
+        if len(con) >= 4:
+            loadavg['lavg_1']=con[0]
+            loadavg['lavg_5']=con[1]
+            loadavg['lavg_15']=con[2]
+            loadavg['nr']=con[3]
+            loadavg['last_pid']=con[4]
+        else:
+            loadavg['lavg_1']="0.00"
+            loadavg['lavg_5']="0.00"
+            loadavg['lavg_15']="0.00"
+            loadavg['nr']="1/25"
+            loadavg['last_pid']="0"
+    else:
+        loadavg['lavg_1']="0.00"
+        loadavg['lavg_5']="0.00"
+        loadavg['lavg_15']="0.00"
+        loadavg['nr']="1/25"
+        loadavg['last_pid']="0"
     return loadavg 
 
+def usernum():
+    if os.path.exists('/var/log/ssnum')  == True:  #用户数文件存在
+        file=open("/var/log/ssnum",'r')    #读取文件
+        for value1 in file.readlines()[:1]:
+            file.close()
+            if value1[:value1.find("\n")] != "":
+                return value1[:value1.find("\n")] #把字符串变成数字
+            else:
+                if value1[:value1.find("\r\n")] != "":
+                    return value1[:value1.find("\r\n")]
+                else:
+                    return "0"
+    else:
+        return "0"
+	
 while 1:
     time.sleep(2)
     tol = psutil.net_io_counters()
@@ -62,10 +110,13 @@ while 1:
     load = load_stat()['lavg_1']
     upload = bytes2human(cul[0] - tol[0])+'/S'
     download = bytes2human(cul[1] - tol[1])+'/S'
-    #os.system("clear")
-   # print 'CPU'+str(cpu)
-   # print '上传速度：'+ upload
-   # print '下载速度：'+download
-   # print '一分钟负载:'+load
-    postdata(load, cpu, upload, download)
+    connections = usernum()
+	
+    os.system("clear")
+    print 'CPU'+str(cpu)
+    print '上传速度：'+ upload
+    print '下载速度：'+download
+    print '一分钟负载:'+load
+    print '用户数:'+ connections
     
+    postdata(load, cpu, upload, download, connections)
