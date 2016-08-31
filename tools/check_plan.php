7e44423b7fdaaf0f03041fb0037b7c7062fb3e85
kot4ri <kot4ri@gmail.com>
2016-08-31 17:59:05 +0800

    Update check_plan.php

--- tools/check_plan.php
+++ tools/check_plan.php
@@ -11,6 +11,40 @@ $smarty->assign('us',$us);
 foreach ($us as $rs){
 $time = $rs['plan_end_time'];
 $now = time();
+$UserInfo = new Ss\User\UserInfo($rs['uid']);
+    
+if($rs['plan'] == 'E'||$rs['plan'] == 'C'){
+    $Users->db->update("user",[
+    "transfer_enable" => "1099511627776"
+        ],[
+        "uid" => $rs['uid']
+        ]);
+}
+
+if( $rs['plan'] == 'D' and $rs['transfer_enable']<='0'){
+        $UserInfo->update_plan_go_time(7);
+        $Users->db->update("user",[
+            "plan"=>'A',
+            "transfer_enable" => "10737418240",
+            "u"=>'0',
+            "d"=>'0'
+        ],[
+            "uid" => $rs['uid']
+        ]);
+    }
+    
+if( $rs['plan'] == 'B' and $rs['transfer_enable']<='0'){
+        $UserInfo->update_plan_go_time(7);
+        $Users->db->update("user",[
+            "plan"=>'A',
+            "transfer_enable" => "2147483648",
+            "u"=>'0',
+            "d"=>'0'
+        ],[
+            "uid" => $rs['uid']
+        ]);
+    }
+
 if ($time<=$now){
     if( $rs['plan'] == 'A'){
         $Users->db->update("user",[
@@ -20,42 +54,26 @@ if ($time<=$now){
         ]);
     }
     if( $rs['plan'] == 'C'){
+        $UserInfo->update_plan_go_time(7);
         $Users->db->update("user",[
             "plan"=>'A',
+            "transfer_enable" => "2147483648",
             "u"=>'0',
-            "d"=>'0',
-            "transfer_enable" => "10737418240"
+            "d"=>'0'
         ],[
             "uid" => $rs['uid']
         ]);
     }
     if( $rs['plan'] == 'E'){
+        $UserInfo->update_plan_go_time(7);
         $Users->db->update("user",[
             "plan"=>'A',
+            "transfer_enable" => "2147483648",
             "u"=>'0',
-            "d"=>'0',
-            "transfer_enable" => "21474836480"
+            "d"=>'0'
         ],[
             "uid" => $rs['uid']
         ]);
     }
 }
-if( $rs['plan'] == 'D' and $rs['transfer_enable']<='0'){
-        $Users->db->update("user",[
-            "plan"=>'A',
-            "u"=>'0',
-            "d"=>'0',
-            "transfer_enable" => "10737418240"
-        ],[
-            "uid" => $rs['uid']
-        ]);
-    }
-if($rs['plan'] == 'E'||$rs['plan'] == 'C'){
-    $Users->db->update("user",[
-    "transfer_enable" => "1072668082176"
-        ],[
-        "uid" => $rs['uid']
-        ]);
 }
-} 
-
