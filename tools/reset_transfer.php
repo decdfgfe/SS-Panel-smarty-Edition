2b9072b4fd5c9362dd95e6be5083cf4b96f18400
kot4ri <kot4ri@gmail.com>
2016-08-31 18:00:32 +0800

    Update reset_transfer.php

--- tools/reset_transfer.php
+++ tools/reset_transfer.php
@@ -1,15 +1,25 @@
 <?php
-/*
- * 清空流量
- */
-//定义清零日期,1为每月1号
-$reset_date = '1';
-//日期符合就清零 
-if (date('d')==$reset_date){
-    $db->update("user",[
-        "u" => "0",
-        "d" => "0",
-        "t"=>  "0",
-        "transfer_enable" => "5368709120"
-    ]);
+//只清零高级套餐流量
+ini_set("display_errors", "On");
+error_reporting(E_ALL | E_STRICT);
+
+require_once '../lib/config.php';
+
+$Users = new Ss\User\User();
+$us = $Users->AllUser();
+$smarty->assign('us',$us);
+
+foreach ($us as $rs){
+$time = $rs['plan_end_time'];
+$now = time();
+$UserInfo = new Ss\User\UserInfo($rs['uid']);
+    
+if($rs['plan'] == 'E'||$rs['plan'] == 'C'){
+    $Users->db->update("user",[
+    "u" => "0",
+	"d" => "0"
+        ],[
+        "uid" => $rs['uid']
+        ]);
+}
 }
