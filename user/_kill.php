ec4901d08423aaa56b69889eae7bb165dbec0289
kot4ri <kot4ri@gmail.com>
2016-10-01 13:40:36 +0800

    Update _kill.php
    添加锁定

--- user/_kill.php
+++ user/_kill.php
@@ -2,19 +2,19 @@
 require_once '../lib/config.php';
 require_once '_check.php';
 // print_r ($_POST);
-if (!empty($_POST['pwd'])) {
-	$pwd = $_POST['pwd'];
-	$pwd = \Ss\User\Comm::SsPW($pwd);
-	
 	if($U->GetPasswd() != $pwd) {
 		    $a['error'] = '1';
 		    $a['msg'] = "密码错误";
 		}else {
-		    $a['ok'] = '1';
-		    $a['msg'] = "再见，您已经安全的从我们的数据库中移除。";
-		    //remove
-		    $U->DelMe();
-
+			if($oo->get_enable()==3) {
+				$a['error'] = '1';
+				$a['msg'] = "账号已被锁定，无法删除。";
+			}else {
+		        $a['ok'] = '1';
+		        $a['msg'] = "再见，你已经安全的从我们的数据库中移除。";
+		        //remove
+		        $U->DelMe();
+            }
 		}
 }else{
 	$a['error'] = '1';
