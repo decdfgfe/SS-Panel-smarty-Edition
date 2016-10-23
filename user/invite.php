d2d7d35ca4e4f76cc84ea180a8dd896fa243b839
C0lacan <c0lacan.inc@gmail.com>
2016-10-23 15:08:17 +0800

    Fix Bug

--- user/invite.php
+++ user/invite.php
@@ -5,6 +5,7 @@ $code = $invite->CodeArray();
 $smarty->assign('InviteNum',$U->InviteNum());
 // $smarty->assign('data',$data);
 $smarty->assign('code',$code);
+$smarty->assign('oo',$oo);
 $smarty->assign('a',$a=1);
 $varsarray = get_defined_vars();
 $smarty->display('user/invite.tpl');
