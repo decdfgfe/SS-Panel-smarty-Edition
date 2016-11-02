f5b7ba6524d00ecc8f1ec1787cbb8df2f517a989
kot4ri <kot4ri@gmail.com>
2016-11-02 12:42:13 +0800

    Update User.php

--- lib/Ss/User/User.php
+++ lib/Ss/User/User.php
@@ -25,7 +25,7 @@ error_reporting(E_ALL);
         return $datas;
      }
 
-     function updateUser($name,$email,$pass,$passwd,$transfer_enable,$invite_num,$plan,$enable,$plan_end_time){
+     function updateUser($name,$email,$pass,$passwd,$transfer_enable,$invite_num,$plan,$enable,$plan_end_time,$message){
          return $this->db->update($this->table,[
              "user_name" => $name,
              "email" => $email,
@@ -36,6 +36,7 @@ error_reporting(E_ALL);
              "enable" => $enable,
              "plan"=> $plan,
              "plan_end_time"=>$plan_end_time,
+			 "message"=>$message,
          ],[
              "uid" => $this->uid
          ]);
